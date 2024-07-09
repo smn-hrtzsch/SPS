59a375da9bd914a48ea029ad49405e6557efffac
Simon Hörtzsch <simon@hoertzsch.de>
2024-07-09 13:11:00 +0200

    test

--- src/ClassLib/GroßerTaschenrechner.cs
+++ src/ClassLib/GroßerTaschenrechner.cs
@@ -1,11 +1,3 @@
 using ClassLib;
 
-public class GroßerTaschenrechner : Taschenrechner
-{
-    public int zahl4 = 0;
-
-    public void Print()
-    {
-        Console.WriteLine("Ich bin dumm :).");
-    }
-}
+public class GroßerTaschenrechner : Taschenrechner{public int zahl4 = 0;public void Print(){Console.WriteLine("Ich bin dumm :).");}public GroßerTaschenrechner(int zahl4){this.zahl4=zahl4;}}
