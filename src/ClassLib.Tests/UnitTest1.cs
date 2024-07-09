ec760c08dc97a527e0c7023e246ce2fad5a6bb4e
Simon Hörtzsch <simon@hoertzsch.de>
2024-07-09 12:22:07 +0200

    added second test class

--- src/ClassLib.Tests/UnitTest1.cs
+++ src/ClassLib.Tests/UnitTest1.cs
@@ -9,4 +9,11 @@ public class UnitTest1
         Taschenrechner taschenrechner = new Taschenrechner();
         Assert.True(taschenrechner.add(1,3)==4);
     }
+
+    [Fact]
+    public void TestSub()
+    {
+        KleinerTaschenrechner kleiner_taschenrechner = new KleinerTaschenrechner();
+        Assert.True(kleiner_taschenrechner.sub(1,3)==-2);
+    }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,5 +6,8 @@ class Program {
         Taschenrechner taschenrechner = new Taschenrechner();
         int result = taschenrechner.add(2,3);
         Console.WriteLine("Result: {0}", result);
+        KleinerTaschenrechner kleiner_taschenrechner = new KleinerTaschenrechner();
+        int result2 = kleiner_taschenrechner.sub(2,3);
+        Console.WriteLine("Result: {0}", result2);
     }
 }
