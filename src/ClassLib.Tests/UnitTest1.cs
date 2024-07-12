bce89f7e1753e1ff5687536d260797b283f28411
Simon Hörtzsch <simon@hoertzsch.de>
2024-07-12 08:57:16 +0200

    resetted to initial file state

--- src/ClassLib.Tests/UnitTest1.cs
+++ src/ClassLib.Tests/UnitTest1.cs
@@ -4,17 +4,4 @@ using ClassLib;
 
 public class UnitTest1
 {
-    [Fact]
-    public void TestAdd()
-    {
-        Taschenrechner taschenrechner = new Taschenrechner();
-        Assert.True(taschenrechner.add(1, 3) == 4);
-    }
-
-    [Fact]
-    public void TestSub()
-    {
-        KleinerTaschenrechner kleiner_taschenrechner = new KleinerTaschenrechner();
-        Assert.True(kleiner_taschenrechner.sub(1, 3) == -2);
-    }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,11 +5,5 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Taschenrechner taschenrechner = new Taschenrechner();
-        int result = taschenrechner.add(2, 3);
-        Console.WriteLine("Result: {0}", result);
-        KleinerTaschenrechner kleiner_taschenrechner = new KleinerTaschenrechner();
-        int result2 = kleiner_taschenrechner.sub(2, 3);
-        Console.WriteLine("Result: {0}", result2);
     }
 }
