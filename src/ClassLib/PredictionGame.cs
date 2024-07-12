71501ea9f5597fec79153dfd674c64a710edcf36
Artim Meyer <artim.meyer@oline.de>
2024-07-12 09:18:43 +0200

    #5 Created PredictionGame-Class

---
+++ src/ClassLib/PredictionGame.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class PredictionGame
+ {
+    public uint PredictionGameID { get;}
+    private static uint PredictionGameIDCounter = 0;
+    private List<Member> Members {get; set;}
+    public List<ScheduleTypes> ScheduleTypes {get;}
+    private EmailService email_service {get; set;}
+    // public PredictionGame (EmailService email_service){}
+    // public void Register(Member member){}
+    // public void Unsubscribe(int MemberID){}
+    // public void SendDailyEmail(){}
+
+ }
