636509165a2add8df8738bada6992cb5431e8fff
Simon Hörtzsch <simon@hoertzsch.de>
2024-07-12 16:18:46 +0200

    #4 Update Schedule class

--- src/ClassLib/Schedule.cs
+++ src/ClassLib/Schedule.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 
+/// \brief Generic class Schedule which represents a tournament.
+/// \details It contains a list of all the matches which take place during the tournament.
+/// \details Added to that it also contains a list of all the matches on the specific day of the tournament.
 public class Schedule<T>
     where T : Match
 {
@@ -8,17 +11,17 @@ public class Schedule<T>
     public List<T> Matches { get; }
     public List<T> MatchesOnDay { get; }
 
-    public Schedule(SchedulTypes schedule_type, string PathToCsvFile)
+    public Schedule(ScheduleTypes schedule_type, string PathToCsvFile)
     {
         // code
     }
 
-    List<Match> GetMatchesFromCsvFile(string PathToCsvFile, ScheduleTypes schedule_type)
+    List<T> GetMatchesFromCsvFile(string PathToCsvFile, ScheduleTypes schedule_type)
     {
         // code
     }
 
-    List<Match> GetMatchesOnDay(DateTime date)
+    List<T> GetMatchesOnDay(DateTime date)
     {
         // code
     }
