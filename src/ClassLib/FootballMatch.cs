51ce7841277feb86a6dbceeb3dd918b44c51a375
Simon Hörtzsch <simon@hoertzsch.de>
2024-07-16 15:43:35 +0200

    #23 Update FootballMatch ctor

--- src/ClassLib/FootballMatch.cs
+++ src/ClassLib/FootballMatch.cs
@@ -4,11 +4,29 @@ public class FootballMatch : Match
 {
     public string? HomeTeam { get; }
     public string? AwayTeam { get; }
+    public byte? ResultHomeTeamPenalties { get; }
+    public byte? ResultAwayTeamPenalties { get; }
     private MatchTypes match_type { get; }
 
-    public FootballMatch(string PathToMatchDataCsvFile)
-        : base(PathToMatchDataCsvFile)
+    public FootballMatch(string PathToMatchDataCsvFile, int line_number)
+        : base(PathToMatchDataCsvFile, line_number)
     {
-        //code
+        this.HomeTeam = MatchArray[1];
+        this.AwayTeam = MatchArray[2];
+        try
+        {
+            this.ResultHomeTeamPenalties = byte.Parse(MatchArray[5]);
+            this.ResultAwayTeamPenalties = byte.Parse(MatchArray[6]);
+        }
+        catch (FormatException)
+        {
+            this.ResultHomeTeamPenalties = null;
+            this.ResultAwayTeamPenalties = null;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.MatchID};{this.MatchDate};{this.HomeTeam};{this.AwayTeam};{this.ResultTeam1};{this.ResultTeam2};{this.ResultHomeTeamPenalties};{this.ResultAwayTeamPenalties}";
     }
 }
