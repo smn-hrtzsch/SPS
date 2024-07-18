dd15a4045d573bffa77b81e21a0a6d51f23886cb
Simon Hörtzsch <77014872+smn-hrtzsch@users.noreply.github.com>
2024-07-18 14:41:51 +0200

    Implement Prediction class functionality (#31)
    * #7 Create Prediction class
    
    * #28 Add ctor, ChangePrediction, ToString()
    
    * #28 Add ctor, ValidatePrediction, GetHashCode,
    ToString()
    
    * #28 Update path to csv file uses
    
    * #28 Add new test class for Prediction

---
+++ src/ClassLib.Tests/TestPredictionClass.cs
@@ -0,0 +1,41 @@
+public class TestPrediction : Prediction
+{
+    public TestPrediction(uint member_id, Match predicted_match, DateTime predictionDate)
+        : base(member_id, predicted_match, predictionDate) { }
+
+    public DateTime PredictionDateTest
+    {
+        get { return PredictionDate; }
+    }
+}
+
+public class TestPredictionClass
+{
+    [Fact]
+    public static void TestFootballPredictionCtor()
+    {
+        FootballMatch match2 = new FootballMatch("../../../../../csv-files/EM_2024.csv", 51);
+        DateTime expectedDate = DateTime.Now;
+        FootballPrediction prediction1 = new FootballPrediction(1, match2, expectedDate, 1, 3);
+        TestPrediction testprediction1 = new TestPrediction(1, match2, expectedDate);
+
+        uint expectedID = (uint)
+            HashCode.Combine(match2.GetHashCode(), testprediction1.PredictionDateTest);
+        string expected_result =
+            $"{expectedID}, 1, {match2.ToString()}, {testprediction1.PredictionDateTest}, 1, 3";
+
+        Assert.True(
+            expected_result == prediction1.ToString(),
+            $"\tExpected:\t{expected_result}\n\tActual:\t\t{prediction1.ToString()}"
+        );
+    }
+
+    [Fact]
+    public static void TestValidatePrediction()
+    {
+        FootballMatch match2 = new FootballMatch("../../../../../csv-files/EM_2024.csv", 51);
+        FootballPrediction prediction2 = new FootballPrediction(1, match2, DateTime.Now, 1, 3);
+
+        Assert.False(prediction2.ValidatePrediction());
+    }
+}
