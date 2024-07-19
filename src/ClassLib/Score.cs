7019bb2f0c3a5bf074a8350e47000435f8df25f4
Artim Meyer <artim.meyer@oline.de>
2024-07-19 16:50:01 +0200

    #26 Reset Score.cs

--- src/ClassLib/Score.cs
+++ src/ClassLib/Score.cs
@@ -3,30 +3,17 @@ using System;
 public class Score
 {
     public uint ScoreID { get; }
+    private static uint ScoreIDCounter = 0;
     private ScheduleTypes PredictedSchedule { get; }
-    public string AmountOfPoints { get; set; } //Zum Beispiel 5:1 oder 6:2 -> kann man dann mit Split umwandeln, siehe GetScoreTeam1 oder GetScoreTeam2
+    private uint AmountOfPoints { get; set; }
 
-    public Score(uint PredictionID, ScheduleTypes PredictedSchedule, string AmountOfPoints)
+    public Score(ScheduleTypes PredictedSchedule)
     {
-        this.ScoreID = PredictionID; //!!!ACHTUNG die ScoreID kennzeichnet die Zugehörigkeit zur Prediction, da die PredictionID und die ScoreID gleich sind (sonst finden wir den Score zur Prediction nicht wieder)
-        this.PredictedSchedule = PredictedSchedule;
-        this.AmountOfPoints = AmountOfPoints;
+        // code
     }
 
     public int CalculateScore(ScheduleTypes PredictedSchedule, Prediction prediction)
     {
         return 0;
-    }
-
-    public uint GetScoreTeam1()
-    {
-        string[] scorearray = AmountOfPoints.Split(':');
-        return uint.Parse(scorearray[0]);
-    }
-
-    public uint GetScoreTeam2()
-    {
-        string[] scorearray = AmountOfPoints.Split(':');
-        return uint.Parse(scorearray[1]); 
     }
 }
