7d11a031667c9ad007435aca85043b82f79895ed
Simon Hörtzsch <simon@hoertzsch.de>
2024-07-20 03:02:18 +0200

    #33 Update test functions with MatchFactory functionality

--- src/ClassLib.Tests/MemberTest.cs
+++ src/ClassLib.Tests/MemberTest.cs
@@ -1,11 +1,11 @@
 using System.Reflection.Metadata.Ecma335;
 
-public class TestMember : Member
+public class TestMember : Member<Prediction, Match>
 {
     public TestMember(string forename, string surname, string emailaddress)
         : base(forename, surname, emailaddress) { }
 
-    public List<Schedule> ParticipatingSchedulesTest
+    public List<Schedule<Match>> ParticipatingSchedulesTest
     {
         get { return ParticipatingSchedules; }
     }
@@ -28,6 +28,8 @@ public class TestMember : Member
 
 public class MemberTest
 {
+    private IMatchFactory<FootballMatch> footballMatchFactory = new FootballMatchFactory();
+
     [Fact]
     public void TestMemberCtor()
     {
@@ -47,16 +49,18 @@ public class MemberTest
         string email = "maria.magdalena@online.de";
 
         TestMember TestMember = new TestMember(vorname, nachname, email);
-        Schedule schedule = new Schedule(
+        CSVReader<FootballMatch>.SetMatchFactory(footballMatchFactory);
+        Schedule<Match> schedule = new Schedule<Match>(
             "../../../EM_2024Test.csv",
             SportsTypes.Football,
             ScheduleTypes.EM_2024
         );
 
-        TestMember.AddParticipatingSchedule(schedule);
+        TestMember.AddParticipatingSchedule(schedule, ScheduleTypes.EM_2024);
         Assert.True(TestMember.ParticipatingSchedulesTest.Count == 1);
     }
 
+    [Fact]
     public void TestMemberRemoveParticipatingSchedule()
     {
         string vorname = "Maria";
@@ -64,17 +68,19 @@ public class MemberTest
         string email = "maria.magdalena@online.de";
 
         TestMember TestMember = new TestMember(vorname, nachname, email);
-        Schedule schedule = new Schedule(
+        CSVReader<FootballMatch>.SetMatchFactory(footballMatchFactory);
+        Schedule<Match> schedule = new Schedule<Match>(
             "../../../EM_2024Test.csv",
             SportsTypes.Football,
             ScheduleTypes.EM_2024
         );
 
-        TestMember.AddParticipatingSchedule(schedule);
+        TestMember.AddParticipatingSchedule(schedule, ScheduleTypes.EM_2024);
         TestMember.RemoveParticipatingSchedule(ScheduleTypes.EM_2024);
         Assert.True(TestMember.ParticipatingSchedulesTest.Count == 0);
     }
 
+    [Fact]
     public void TestMemberAddPredictionToDo()
     {
         string vorname = "Maria";
@@ -82,13 +88,14 @@ public class MemberTest
         string email = "maria.magdalena@online.de";
 
         TestMember TestMember = new TestMember(vorname, nachname, email);
-        Schedule schedule = new Schedule(
+        CSVReader<FootballMatch>.SetMatchFactory(footballMatchFactory);
+        Schedule<Match> schedule = new Schedule<Match>(
             "../../../EM_2024Test.csv",
             SportsTypes.Football,
             ScheduleTypes.EM_2024
         );
 
-        TestMember.AddParticipatingSchedule(schedule);
+        TestMember.AddParticipatingSchedule(schedule, ScheduleTypes.EM_2024);
         TestMember.AddPredictionToDo();
         Assert.True(
             TestMember.PredictionsToDoTest.Count == schedule.GetMatchesOnDay().Count,
@@ -96,6 +103,7 @@ public class MemberTest
         );
     }
 
+    [Fact]
     public void TestMemberRemovePredictionToDo()
     {
         string vorname = "Maria";
@@ -104,8 +112,16 @@ public class MemberTest
 
         TestMember TestMember = new TestMember(vorname, nachname, email);
 
-        FootballMatch match1 = new FootballMatch("../../../EM_2024Test.csv", 1);
-        FootballMatch match2 = new FootballMatch("../../../EM_2024Test.csv", 51);
+        FootballMatch match1 = new FootballMatch(
+            "../../../EM_2024Test.csv",
+            1,
+            SportsTypes.Football
+        );
+        FootballMatch match2 = new FootballMatch(
+            "../../../EM_2024Test.csv",
+            51,
+            SportsTypes.Football
+        );
 
         TestMember.PredictionsToDoTest.Add(match1);
         TestMember.PredictionsToDoTest.Add(match2);
@@ -115,6 +131,7 @@ public class MemberTest
         Assert.True(TestMember.PredictionsToDoTest.Count == 1);
     }
 
+    [Fact]
     public void TestMemberSearchPrediction() //TODO
     { }
 }
