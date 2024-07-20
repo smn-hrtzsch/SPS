62c302368ced5c642a7da827846f8fac0f951183
Simon Hörtzsch <simon@hoertzsch.de>
2024-07-20 02:52:42 +0200

    #33 Update ctor and Add InitializeMatch()

--- src/ClassLib/FootballMatch.cs
+++ src/ClassLib/FootballMatch.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 public class FootballMatch : Match
 {
@@ -8,33 +9,58 @@ public class FootballMatch : Match
     public byte? ResultAwayTeamPenalties { get; }
     private MatchTypes match_type { get; }
 
-    public FootballMatch(string PathToMatchDataCsvFile, int line_number)
-        : base(PathToMatchDataCsvFile, line_number)
+    /// \brief Parameter free constructor to use generic type M in <see cref="CSVReader"/>
+    public FootballMatch() { }
+
+    public FootballMatch(string PathToMatchDataCsvFile, int line_number, SportsTypes sport_type)
     {
-        this.HomeTeam = MatchArray[1];
-        this.AwayTeam = MatchArray[2];
+        InitializeMatch(PathToMatchDataCsvFile, line_number, sport_type);
+        HomeTeam = MatchArray[1];
+        AwayTeam = MatchArray[2];
         try
         {
-            this.ResultHomeTeamPenalties = byte.Parse(MatchArray[5]);
-            this.ResultAwayTeamPenalties = byte.Parse(MatchArray[6]);
+            ResultHomeTeamPenalties = byte.Parse(MatchArray[5]);
+            ResultAwayTeamPenalties = byte.Parse(MatchArray[6]);
         }
         catch (FormatException)
         {
-            this.ResultHomeTeamPenalties = null;
-            this.ResultAwayTeamPenalties = null;
+            ResultHomeTeamPenalties = null;
+            ResultAwayTeamPenalties = null;
         }
+    }
+
+    private void InitializeMatch(
+        string PathToMatchDataCsvFile,
+        int line_number,
+        SportsTypes sport_type
+    )
+    {
+        MatchArray = CSVReader<FootballMatch>.GetMatchDataFromCsvFile(
+            PathToMatchDataCsvFile,
+            line_number
+        );
+
+        MatchDate = DateTime.ParseExact(
+            MatchArray[0],
+            "dd/MM/yyyy HH:mm",
+            CultureInfo.InvariantCulture
+        );
+        ResultTeam1 = byte.Parse(MatchArray[3]);
+        ResultTeam2 = byte.Parse(MatchArray[4]);
+        SportsType = sport_type;
+        MatchID = (uint)GetHashCode();
     }
 
     public override string ToString()
     {
-        string mi = $"{this.MatchID}";
-        string md = $"{this.MatchDate}";
-        string ht = $"{this.HomeTeam}";
-        string at = $"{this.AwayTeam}";
-        string rt1 = $"{this.ResultTeam1}";
-        string rt2 = $"{this.ResultTeam2}";
-        string rhtp = $"{this.ResultHomeTeamPenalties}";
-        string ratp = $"{this.ResultAwayTeamPenalties}";
+        string mi = $"{MatchID}";
+        string md = $"{MatchDate}";
+        string ht = $"{HomeTeam}";
+        string at = $"{AwayTeam}";
+        string rt1 = $"{ResultTeam1}";
+        string rt2 = $"{ResultTeam2}";
+        string rhtp = $"{ResultHomeTeamPenalties}";
+        string ratp = $"{ResultAwayTeamPenalties}";
         return $"{mi};{md};{ht};{at};{rt1};{rt2};{rhtp};{ratp}";
     }
 }
