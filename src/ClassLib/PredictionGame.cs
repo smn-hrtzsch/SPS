27d5850a1dcf927603fcd1516d0ffbd0d44ad141
Artim Meyer <artim.meyer@oline.de>
2024-07-22 19:49:50 +0200

    #36 United email templates into one daily email

--- src/ClassLib/PredictionGame.cs
+++ src/ClassLib/PredictionGame.cs
@@ -25,7 +25,7 @@ public class PredictionGame
         email_service = emailService;
         PredictionGameID = (uint)GetHashCode();
         Members = new List<Member<Prediction, Match>>();
-        ScheduleTypesList = ScheduleTypesList;
+        ScheduleTypesList = GetAllScheduleTypes();
     }
 
     private List<ScheduleTypes> GetAllScheduleTypes()
@@ -63,35 +63,29 @@ public class PredictionGame
 
     /// \brief Sends a daily email to all members with the matches that need to be predicted.
 
-    public void SendRoutineEmail(EmailTypes emailTypes)
+    public void SendDailyEmail()
     {
         string TippTemplate =
             @"
         <html>
-            <body style='font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;'>
-                <h1 style='color: #444;'>Hallo {{forename}},</h1>
-                <p style='font-size: 16px; color: #666;'>Bereit für ein aufregendes Spielabenteuer? Hier sind deine exklusiven Tipps für heute:</p>
-                <ul style='list-style-type: none; padding: 0;'>
-                    {{matches}}
-                </ul>
-                <p style='font-size: 16px; color: #666;'>Spüre den Nervenkitzel, gebe Deine Tipps ab, und lass dich von deinem Instinkt leiten. Klicke einfach auf diesen <a href='{{link}}' style='color: #ff6347; text-decoration: none;'>magischen Link</a> und tauche ein in die Welt von SportsPredictionSystem.</p>
-                <p style='font-size: 16px; color: #666;'>Viel Erfolg und genieße den Moment!</p>
-            </body>
-        </html>";
+    <body style='font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;'>
+        <h1 style='color: #444;'>Hallo {{forename}},</h1>
+        <p style='font-size: 16px; color: #666;'>Bereit, deine Erfolge zu feiern? Hier sind die faszinierenden Ergebnisse der letzten Spiele und die Punkte, die du erreicht hast:</p>
+        <ul style='list-style-type: none; padding: 0;'>
+            {{results}}
+        </ul>
+        <p style='font-size: 16px; color: #666;'>Deine Gesamtpunkte: <strong>{{totalPoints}}</strong></p>
+        <p style='font-size: 16px; color: #666;'>Bleib am Ball und genieße das Tippspiel. Weiter so und viel Erfolg!</p>
 
-        string ResultTemplate =
-            @"
-        <html>
-            <body style='font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;'>
-                <h1 style='color: #444;'>Hallo {{forename}},</h1>
-                <p style='font-size: 16px; color: #666;'>Bereit, deine Erfolge zu feiern? Hier sind die faszinierenden Ergebnisse der letzten Spiele und die Punkte, die du erreicht hast:</p>
-                <ul style='list-style-type: none; padding: 0;'>
-                    {{results}}
-                </ul>
-                <p style='font-size: 16px; color: #666;'>Deine Gesamtpunkte: <strong>{{totalPoints}}</strong></p>
-                <p style='font-size: 16px; color: #666;'>Bleib am Ball und genieße das Tippspiel. Weiter so und viel Erfolg!</p>
-            </body>
-        </html>";
+        <h2 style='color: #444;'>Bereit für ein aufregendes Spielabenteuer?</h2>
+        <p style='font-size: 16px; color: #666;'>Hier sind deine exklusiven Tipps für heute:</p>
+        <ul style='list-style-type: none; padding: 0;'>
+            {{matches}}
+        </ul>
+        <p style='font-size: 16px; color: #666;'>Spüre den Nervenkitzel, gebe deine Tipps ab, und lass dich von deinem Instinkt leiten. Klicke einfach auf diesen <a href='{{link}}' style='color: #ff6347; text-decoration: none;'>magischen Link</a> und tauche ein in die Welt von SportsPredictionSystem.</p>
+        <p style='font-size: 16px; color: #666;'>Viel Erfolg und genieße den Moment!</p>
+    </body>
+</html>";
 
         foreach (var schedule in ScheduleTypesList)
         {
@@ -101,9 +95,6 @@ public class PredictionGame
                 string resultsList = string.Empty;
                 uint totalPoints = 0;
 
-                switch (emailTypes)
-                {
-                    case EmailTypes.TippTemplate:
                         foreach (var match in member.GetPredictionsToDo())
                         {
                             if (schedule == ScheduleTypes.EM_2024)
@@ -112,22 +103,6 @@ public class PredictionGame
                                     $"<li style='font-size: 16px; color: #444; margin: 5px 0;'>{(match as FootballMatch).HomeTeam} vs  {(match as FootballMatch).HomeTeam} am {(match as FootballMatch).MatchDate.ToString("dd.MM.yyyy HH:mm")}</li>";
                             }
                         }
-                        Dictionary<string, string> TippDic = new Dictionary<string, string>
-                        {
-                            { "forename", member.GetForename() },
-                            { "matches", matchesList },
-                            { "link", "https://github.com/smn-hrtzsch/SPS" }
-                        };
-                        email_service.SendEmail(
-                            member.GetEmailAddress(),
-                            "sportspredictionsystem@gmail.com",
-                            "Klick hier um zu Tippen! ;D",
-                            TippTemplate,
-                            TippDic
-                        );
-                        break;
-
-                    case EmailTypes.ResultTemplate:
                         foreach (var score in member.GetScores())
                         {
                             switch (score.ScoreID)
@@ -150,24 +125,22 @@ public class PredictionGame
                                     break;
                             }
                         }
-                        Dictionary<string, string> ResultDic = new Dictionary<string, string>
+                        Dictionary<string, string> TippDic = new Dictionary<string, string>
                         {
                             { "forename", member.GetForename() },
+                            { "matches", matchesList },
+                            { "link", "https://github.com/smn-hrtzsch/SPS" },
                             { "results", resultsList },
                             { "totalPoints", totalPoints.ToString() }
                         };
                         email_service.SendEmail(
                             member.GetEmailAddress(),
                             "sportspredictionsystem@gmail.com",
-                            "Schau dir Deine Erfolge an!",
-                            ResultTemplate,
-                            ResultDic
+                            "Klick hier um zu Tippen! ;D",
+                            TippTemplate,
+                            TippDic
                         );
-                        break;
-
-                    default:
-                        break;
-                }
+                
             }
         }
     }
