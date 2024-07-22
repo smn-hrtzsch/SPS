06849853c24f21a81a513771a47fa7b9b4672e1d
Artim Meyer <artim.meyer@oline.de>
2024-07-22 11:53:57 +0200

    #36 Edited Programm.cs for CI-pipeline implementation, Added comments for structuring and better understanding of the code

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
-    public static void Main(string[] args) { }
+    public static void Main(string[] args) 
+    {
+        //Gerneral Programm variable declaration
+        EmailService emailService = new EmailService();
+        List<ScheduleTypes> scheduleTypes = new List<ScheduleTypes>() {ScheduleTypes.EM_2024};
+        PredictionGame predictionGame = new PredictionGame(emailService, scheduleTypes /*,PathToCSVFile* <- zum Einlesen der Daten in den Member Konstruktor/);
+
+        //Email continuous Integration
+
+        DateTime dateTimeNow = DateTime.Now;
+        DateTime dateTimeAtNineThirty = new DateTime(dateTimeNow.Year, dateTimeNow.Month, dateTimeNow.Day, 9, 30, 0);
+        DateTime dateTimeAtEighteenOClock = new DateTime(dateTimeNow.Year, dateTimeNow.Month, dateTimeNow.Day, 18, 0, 0);
+        
+
+        if(dateTimeNow == dateTimeAtNineThirty) //get daily Tipp-email
+        {
+            predictionGame.SendRoutineEmail(EmailTypes.TippTemplate);   
+        }
+
+        if(dateTimeNow.DayOfWeek == DayOfWeek.Sunday && dateTimeNow == dateTimeAtEighteenOClock) //get Results-email once a week
+        {
+            predictionGame.SendRoutineEmail(EmailTypes.ResultTemplate);
+        }
+
+        //Email continous Integration end
+
+
+    }
 }
