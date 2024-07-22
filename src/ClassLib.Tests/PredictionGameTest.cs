7781c538920e30eaf06bc16ac0f3f10bfb40f0d6
Artim Meyer <artim.meyer@oline.de>
2024-07-22 11:52:46 +0200

    #36 Integrated the ConverPredictionsDone() method into the PredictionGame test, Implemented Get()-methods according to IMemberData, Added Test for both Email-templates

--- src/ClassLib.Tests/PredictionGameTest.cs
+++ src/ClassLib.Tests/PredictionGameTest.cs
@@ -18,28 +18,30 @@ public class PredictionGameTest
             ScheduleTypes.EM_2024
         );
 
-        TestMember member1 = new TestMember("Artim", "Meyer", "artimmeyer@gmail.com ");
-        //TestMember member2 = new TestMember("Artim Werner", "Meyer","Artim-Werner.Meyer@student.tu-freiberg.de");
+        TestMember member1 = new TestMember("Artim", "Meyer", "Artim-Werner.Meyer@student.tu-freiberg.de");
+        //MemberData member2 = new MemberData("Simon", "Hörtzsch", "Simon.Hoertzsch@student.tu-freiberg.de");
 
         predictionGame.Register(member1);
 
         FootballMatch match1 = new FootballMatch("../../../EM_2024Test.csv", 1, SportsTypes.Football);
         FootballMatch match2 = new FootballMatch("../../../EM_2024Test.csv", 51, SportsTypes.Football);
-        FootballPrediction prediction1 = new FootballPrediction(member1.MemberID, match1, match1.MatchDate, 5,1);
-        FootballPrediction prediction2 = new FootballPrediction(member1.MemberID, match2, match2.MatchDate, 2,1);
         
         member1.AddParticipatingSchedule(schedule, ScheduleTypes.EM_2024);
         member1.AddPredictionToDo();
-        member1.PredictionsDoneTest.Add(prediction1);
-        member1.PredictionsDoneTest.Add(prediction2);
+        member1.PredictionsToDoTest.Add(match1);
+        member1.PredictionsToDoTest.Add(match2);
+        member1.ConvertPredictionsDone(match1.MatchID, 5, 1);
+        member1.ConvertPredictionsDone(match2.MatchID, 2, 1);
+
         member1.CalculateScores();
-        Assert.True(member1.ArchivedPredictionsTest.Count == 2);
-        Assert.True(member1.ScoresTest.Count() == 1);
-        Assert.True(member1.ScoresTest.First().ScoreID == ScheduleTypes.EM_2024);
+        Assert.True(member1.GetArchivedPredictions().Count == 2);
+        Assert.True(member1.GetScores().Count() == 1);
+        Assert.True(member1.GetScores().First().ScoreID == ScheduleTypes.EM_2024);
         
         //predictionGame.Register(member2);
 
 
-        predictionGame.SendDailyEmail(EmailTypes.ResultTemplate);
+        predictionGame.SendRoutineEmail(EmailTypes.TippTemplate);
+        predictionGame.SendRoutineEmail(EmailTypes.ResultTemplate);
     }
 }
