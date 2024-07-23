9198104cbda3242815992982d93d325bafe4ce8d
Artim Meyer <artim.meyer@oline.de>
2024-07-23 15:28:28 +0200

    #42 Modified both main functions of the project by enhancing the email continuous integration Program.cs file

--- src/EmailContinuousIntegration/Program.cs
+++ src/EmailContinuousIntegration/Program.cs
@@ -11,11 +11,45 @@ class Program
             emailService /*,PathToCSVFile  <- zum Einlesen der Daten in den Member Konstruktor*/
         );
 
+        Member <Prediction, Match> member = new Member<Prediction, Match>("SportsPrediction", "System", "sportspredictionsystem@gmail.com", "1234");
+        predictionGame.Register(member);
+
         //Email continuous Integration
 
-        predictionGame.SendRoutineEmail();
-
-        Console.WriteLine("Hallo Simon");
+        predictionGame.SendDailyEmail();
         //Email continous Integration end
     }
 }
+
+
+// //Email continuous Integration
+
+//         DateTime dateTimeNow = DateTime.Now;
+//         DateTime dateTimeAtNineThirty = new DateTime(
+//             dateTimeNow.Year,
+//             dateTimeNow.Month,
+//             dateTimeNow.Day,
+//             9,
+//             30,
+//             0
+//         );
+//         DateTime dateTimeAtEighteenOClock = new DateTime(
+//             dateTimeNow.Year,
+//             dateTimeNow.Month,
+//             dateTimeNow.Day,
+//             18,
+//             0,
+//             0
+//         );
+
+//         if (dateTimeNow == dateTimeAtNineThirty) //get daily Tipp-email
+//         {
+//             predictionGame.SendDailyEmail();
+//         }
+
+//         if (dateTimeNow.DayOfWeek == DayOfWeek.Sunday && dateTimeNow == dateTimeAtEighteenOClock) //get Results-email once a week
+//         {
+//             predictionGame.SendDailyEmail();
+//         }
+
+//         //Email continous Integration end
